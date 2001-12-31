c69c26aa65dfcbf10aca87f89869965454e25f4f
harishking809 <harishking809@gmail.com>
2002-01-01 01:42:13 +0530

    modified the wepack.json file for porduction mode

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import moment from "moment";
 import {Provider} from "react-redux";
 import AppRouter from "./Routes/AppRouter"; 
-import configStore from "./Store/configStore";
-import  {addExpense} from "./actions/Expense";
-import {setTextFilter} from "./actions/Filter";
-import getVisibleExpense from "./VisibleExpenseSelector/Expense";
-import 'react-dates/lib/css/_datepicker.css';
-import 'react-dates/lib/css/_datepicker.css';
+import configStore from "./Store/configStore"; 
+import 'react-dates/lib/css/_datepicker.css'; 
 import "./styles/styles.scss";
 const store=configStore();
+
+console.log("im testing my wode")
 
 const jsx=(
     <Provider store={store}>
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,60 @@
 const path=require("path"); 
+const miniCssExtractPlugin =require("mini-css-extract-plugin"); 
 
-module.exports={
-    entry:"./src/app.js",
-    output:{
-        filename:"bundle.js",
-        path:path.join(__dirname,"public")
-    },
-    module:{
-     rules:[{
-         loader:"babel-loader",
-         test:/\.js$/,
-         exclude:/node_modules/
-     },{
-         test:/\.s?css$/,
-         use:[
-             "style-loader",
-             "css-loader",
-             "sass-loader",
-         ],
-     }]
-    },
-    // devtool:"eval-cheap-module-source-map", // for debugging the code
-    mode:"development",
-    devServer:{
-        contentBase:path.join(__dirname,"public"),
-        port:9000,
-        historyApiFallback:true
-    }
-};
+module.exports=(env)=>{  
+    // console messages
+    console.log(env.production?
+    "its look like we are running build in porduction mode"
+    :"its look like we are running build in development mode");
+
+    // return body
+    return {
+        entry:"./src/app.js",
+        output:{
+            filename:"bundle.js",
+            path:path.join(__dirname,"public")
+        },
+        module:{
+         rules:[
+        {
+             loader:"babel-loader",
+             test:/\.js$/,
+             exclude:/node_modules/
+         }, 
+         {
+            test:/\.s?css$/,
+            use : [
+              miniCssExtractPlugin.loader,
+              {
+                loader :'css-loader',
+                options:{
+                   sourceMap:true,
+                    url:false
+                }
+              },
+              {
+                loader:'sass-loader',
+                options:{
+                  sourceMap:true
+                }
+              } 
+            ] 
+          } 
+        ]
+        },
+       devtool:env.production?"source-map":"inline-source-map", // for debugging the code
+        mode:env.production?"production":"development",
+        devServer:{
+            contentBase:path.join(__dirname,"public"),
+            port:9000,
+            historyApiFallback:true
+        },
+        plugins : [
+            new miniCssExtractPlugin({
+              filename:"styles.css"
+            })
+        ]
+    };
+}
 
 // devtool:"eval", // for build code fastly
-//or
